4d597bb658cd993d0315fefdbc9bf9d7025b8464
kristenmills <kristen@kristen-mills.com>
2013-07-04 13:18:48 -0700

    added code to the event and transition classes

--- lib/finite/event.rb
+++ lib/finite/event.rb
@@ -2,6 +2,40 @@ module Finite
 
   # The event class. Represents an event in the state machine
   class Event
-    # some code
+
+    # Create an event object
+    #
+    # @param name [Symbol] the name of the event
+    # @param block [Block] the block of code in the event
+    def initialize(name, &block)
+      @name = name
+      @transitions = []
+      instance_eval &block
+    end
+
+    # Are two events equal
+    #
+    # @param other [Object] the object you are comparing it to
+    # @return true if they are equal false if not
+    def ==(other)
+      if other.is_a? Event
+        @name == event.name
+      else
+        false
+      end
+    end
+
+    # The transition method for the dsl
+    #
+    # @param opts [Hash] the options for a transition
+    def go(opts)
+      #Some code
+    end
+
+    [:after, :before].each do |callback|
+      define_method callback do |*args, &block|
+        #some code
+      end
+    end
   end
 end
--- lib/finite/transition.rb
+++ lib/finite/transition.rb
@@ -2,6 +2,15 @@ module Finite
 
   # The transition class. Represents a transition between two states
   class Transition
-    # some code
+
+    attr_reader :to, :from
+
+    # Create a new transition object
+    #
+    # @param opts [Hash] the options for a transition. Include :to and :from
+    def initialize(opts)
+      @from = opts[:from]
+      @to = opts[:to]
+    end
   end
 end
